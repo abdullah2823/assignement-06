11ff21b8dbe2662979dfb718c3d861dc98ae6c22
mohammad abdullah <mdabdullah50a@gmail.com>
2025-09-09 22:03:28 +0600

    tree card section add

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
+
+
 const treeCatergoris = document.getElementById("tree-categories");
 const allPlantsDiv = document.getElementById("all-plants");
+const allCardDiv = document.getElementById("all-plants")
+
+const addTocard = [];
+
 
 const catagorisTree = () => {
     fetch("https://openapi.programming-hero.com/api/categories")
@@ -10,6 +16,7 @@ const catagorisTree = () => {
         })
         .catch(err => console.log(err));
 };
+
 
 const displayCategories = (treeDiv) => {
     treeDiv.forEach(cat => {
@@ -24,11 +31,13 @@ const displayCategories = (treeDiv) => {
         `;
     });
 
+
     treeCatergoris.addEventListener('click', (e) => {
         const allli = document.querySelectorAll("li");
         allli.forEach(li => {
             li.classList.remove("bg-green-800", "text-white", "rounded-md");
         });
+
 
         const li = e.target.closest("li");
         if (li) {
@@ -39,6 +48,8 @@ const displayCategories = (treeDiv) => {
 };
 
 
+
+
 const loadAllPlants = () => {
     fetch("https://openapi.programming-hero.com/api/categories")
         .then(res => res.json())
@@ -47,13 +58,42 @@ const loadAllPlants = () => {
                 fetch(`https://openapi.programming-hero.com/api/category/${cat.id}`)
                     .then(res => res.json())
                     .then(plantsData => {
-                        allPlantsShow(plantsData.plants); 
+                        allPlantsShow(plantsData.plants);
                     });
             });
         });
 };
 
 
+const loadcardDiteails = (id) => {
+    const url = `https://openapi.programming-hero.com/api/plant/${id}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then(data => {
+            displaycardDiteails(data.plants)
+        })
+}
+
+
+const displaycardDiteails = (word) => {
+    console.log(word)
+    const detailseBox = document.getElementById("detielseContiner");
+    detailseBox.innerHTML = `
+                    <img class="rounded-lg w-full h-70 object-cover mx-auto rounded-2xl"
+                            src="${word.image}" alt="">
+
+                        <div>
+                            <h1 class="text-[20px] font-semibold ">${word.name}</h1>
+                            <p class="text-gray-500">${word.description}</p>
+                        </div>
+                        <div class="flex justify-between">
+                            <h2 class=" font-semibold p-3 bg-green-100">${word.category}</h2>
+                            <p class="text-xl font-semibold">${word.price}</p>
+                        </div>`;
+    document.getElementById("my_modal_5").showModal();
+
+
+}
 const allPlants = (plantsId) => {
     allPlantsDiv.innerHTML = "";
     fetch(`https://openapi.programming-hero.com/api/category/${plantsId}`)
@@ -63,6 +103,8 @@ const allPlants = (plantsId) => {
         })
         .catch(err => console.log(err));
 };
+
+
 
 
 const allPlantsShow = (plants) => {
@@ -75,7 +117,7 @@ const allPlantsShow = (plants) => {
                         src="${plants.image}" alt="">
                 </figure>
                 <div>
-                    <h1 class="text-[16px] font-semibold ">${plants.name}</h1>
+                    <h1 id="${plants.id}" class="text-[16px] font-semibold "  onclick="loadcardDiteails(${plants.id})">${plants.name}</h1>
                     <p class="text-gray-500">${plants.description}</p>
                 </div>
                 <div class="flex justify-between items-center">
@@ -93,6 +135,32 @@ const allPlantsShow = (plants) => {
     });
 };
 
+
+allCardDiv.addEventListener('click', (e) =>{
+   
+    if(e.target.innerText === "Add to Cart" )
+        
+        showcards(e);
+     
+})
+
+const showcards = (e) =>{
+    const titelName = e.target.parentNode.children[1].children[0].innerText;
+    const id = e.target.parentNode;
+    addTocard.push({
+        titelName:titelName,
+        id:id
+    })
+    displaycard(addTocard)
+
+}
+
+const displaycard = (addTocard) =>{
+    allCardDiv.forEach(addTocard =>{
+        allCardDiv.innerHTML += ``
+    })
+
+}
 
 catagorisTree();
 loadAllPlants();
